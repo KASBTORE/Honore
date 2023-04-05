79e15001a9d8ae31acef5412400ad6a267d42860
Ernor1 <honorerukundo74@gmail.com>
2023-04-05 22:32:35 +0200

    Updates

--- dist/next-sitemap.config.dev.js
+++ dist/next-sitemap.config.dev.js
@@ -1,7 +1,10 @@
+"use strict";
+
 /** @type {import('next-sitemap').IConfig} */
 // Default code you can customize according to your requirements.
 module.exports = {
-    siteUrl: 'https://kabstore.vercel.app',
-    generateRobotsTxt: true, // (optional)
-    // REST CODE READ DOCS  ...
-}
+  siteUrl: 'https://kabstore.vercel.app',
+  generateRobotsTxt: true // (optional)
+  // REST CODE READ DOCS  ...
+
+};
