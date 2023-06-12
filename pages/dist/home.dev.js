d7c32e10ebc7c3413d2aa4b49a825001fa15b55a
Ernor1 <honorerukundo74@gmail.com>
2023-06-12 18:00:22 +0200

    Updates

--- pages/dist/home.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _Header = _interopRequireDefault(require("components/Header1"));
-
-var _Footer = _interopRequireDefault(require("components/Footer"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
